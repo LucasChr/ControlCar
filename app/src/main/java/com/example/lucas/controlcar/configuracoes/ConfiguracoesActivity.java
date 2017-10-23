077c35682ac7739a3d2992b0dec436f679251839
Lucas <lucaschr37@gmail.com>
2017-10-23 09:44:43 -0200

    Desenvolvimento do dia

--- app/src/main/java/com/example/lucas/controlcar/configuracoes/ConfiguracoesActivity.java
+++ app/src/main/java/com/example/lucas/controlcar/configuracoes/ConfiguracoesActivity.java
@@ -7,6 +7,8 @@ import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -25,22 +27,33 @@ import com.example.lucas.controlcar.bluetooth.ReceberMensagemActivity;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.UUID;
 
 public class ConfiguracoesActivity extends AppCompatActivity {
 
+    private Intent intent;
+
     Button btnConectar;
+
+    private static final int SOLICITA_ATIVACAO = 1;
+    private static final int SOLICITA_CONEXAO = 2;
+    private static final int MESSAGE_READ = 3;
+
+    ConnectedThread connectedThread;
+
+    Handler mHandler;
+    StringBuilder dadosBluetooth = new StringBuilder();
+
     BluetoothAdapter btfAdapter = null;
     BluetoothDevice btfDevice = null;
     BluetoothSocket btfSocket = null;
+
     boolean conexao = false;
-    private static final int SOLICITA_ATIVACAO = 1;
-    private static final int SOLICITA_CONEXAO = 2;
+
     private static String MAC = null;
+
     UUID btf_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-    private Intent intent;
-    private boolean running;
-    private BluetoothServerSocket serverSocket;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -78,6 +91,18 @@ public class ConfiguracoesActivity extends AppCompatActivity {
                 }
             }
         });
+
+        mHandler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+
+                if(msg.what == MESSAGE_READ){
+
+                }
+
+            }
+
+        };
     }
 
     @Override
@@ -97,12 +122,14 @@ public class ConfiguracoesActivity extends AppCompatActivity {
                     try {
                         btfSocket = btfDevice.createRfcommSocketToServiceRecord(btf_UUID);
                         btfSocket.connect();
+
                         conexao = true;
+
+                        connectedThread = new ConnectedThread(btfSocket);
+                        connectedThread.start();
+
                         btnConectar.setText("Desconectar");
-                        if (btfAdapter != null && btfAdapter.isEnabled()) {
-                            new ThreadServidor().start();
-                            running = true;
-                        }
+
                         Toast.makeText(getApplicationContext(), "Conecado com: " + MAC, Toast.LENGTH_LONG).show();
                     } catch (IOException e) {
                         conexao = false;
@@ -143,88 +170,144 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         finishAffinity();
     }
 
-    //Thread para controlar a conexao e nao travar a tela
-    public class ThreadServidor extends Thread {
-        private String TAG = "Control car";
+    private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
 
-        @Override
-        public void run() {
-            super.run();
+        public ConnectedThread(BluetoothSocket socket) {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
             try {
-                //Abre o socket servidor (quem for conectar precisa utilizar o mesmo UUID)
-                BluetoothServerSocket serverSocket = btfAdapter.listenUsingRfcommWithServiceRecord("Bluetooth", btf_UUID);
-                //Fica aguardando alguem conectar (Chamada bloqueante)
-                try {
-                    //Aguarda conexoes
-                    btfSocket = serverSocket.accept();
-                } catch (Exception e) {
-                    //Em caso de erro encerrar aqui
-                    return;
-                }
-
-                if (btfSocket != null) {
-                    //Alguem conectou entao encerra-se o socket server
-                    serverSocket.close();
-                    //O socket possui a InputStream e OutputStram para ler e escrever
-                    InputStream in = btfSocket.getInputStream();
-                    //Recupera o dispositivo cliente que fez a conexao
-                    btfDevice = btfSocket.getRemoteDevice();
-                    updateViewConectou(btfDevice);
-                    byte[] bytes = new byte[1024];
-                    int length;
-                    //Fica em loop para receber as mensagens
-                    while (running) {
-                        //Le a mensagem (chamada bloqueada até alguem escrever)
-                        length = in.read(bytes);
-                        String mensagemRecebida = new String(bytes, 0, length);
-                        TextView tvMsg = (TextView) findViewById(R.id.tvMsg);
-                        final String s = tvMsg.getText().toString() + mensagemRecebida + "\n";
-                        updateViewMensagem(s);
-                    }
-                }
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
             } catch (IOException e) {
-                Log.e(TAG, "Erro no servidor: " + e.getMessage(), e);
-                running = false;
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.read(buffer);
+
+                    String dadosBtf = new String(buffer, 0, bytes);
+
+                    // Send the obtained bytes to the UI activity
+                    mHandler.obtainMessage(MESSAGE_READ, bytes, -1, dadosBtf).sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
             }
         }
+
+//        /* Call this from the main activity to send data to the remote device */
+//        public void write(byte[] bytes) {
+//            try {
+//                mmOutStream.write(bytes);
+//            } catch (IOException e) { }
+//        }
+//        /* Call this from the main activity to shutdown the connection */
+//        public void cancel() {
+//            try {
+//                btfSocket.close();
+//            } catch (IOException e) {
+//            }
+//        }
     }
 
-    //Exibe a mensagem na tela, informando o nome do dispositivo que fez a conexao
-    private void updateViewConectou(final BluetoothDevice device) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                TextView tvNome = (TextView) findViewById(R.id.tvNomeDispositivo);
-                tvNome.setText(device.getName() + " - " + device.getAddress());
-            }
-        });
-    }
 
-    //Exibe a mensagem recebida na tela (Enviada pelo outro dispositivo)
-    private void updateViewMensagem(final String s) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                TextView tvMsg = (TextView) findViewById(R.id.tvMsg);
-                tvMsg.setText(s);
-            }
-        });
-    }
+//    //Thread para controlar a conexao e nao travar a tela
+//    public class ThreadServidor extends Thread {
+//        private String TAG = "Control car";
+//
+//        @Override
+//        public void run() {
+//            super.run();
+//            try {
+//                //Abre o socket servidor (quem for conectar precisa utilizar o mesmo UUID)
+//                BluetoothServerSocket serverSocket = btfAdapter.listenUsingRfcommWithServiceRecord("Bluetooth", btf_UUID);
+//                //Fica aguardando alguem conectar (Chamada bloqueante)
+//                try {
+//                    //Aguarda conexoes
+//                    btfSocket = serverSocket.accept();
+//                } catch (Exception e) {
+//                    //Em caso de erro encerrar aqui
+//                    return;
+//                }
+//
+//                if (btfSocket != null) {
+//                    //Alguem conectou entao encerra-se o socket server
+//                    serverSocket.close();
+//                    //O socket possui a InputStream e OutputStram para ler e escrever
+//                    InputStream in = btfSocket.getInputStream();
+//                    //Recupera o dispositivo cliente que fez a conexao
+//                    btfDevice = btfSocket.getRemoteDevice();
+//                    updateViewConectou(btfDevice);
+//                    byte[] bytes = new byte[1024];
+//                    int length;
+//                    //Fica em loop para receber as mensagens
+//                    while (running) {
+//                        //Le a mensagem (chamada bloqueada até alguem escrever)
+//                        length = in.read(bytes);
+//                        String mensagemRecebida = new String(bytes, 0, length);
+//                        TextView tvMsg = (TextView) findViewById(R.id.tvMsg);
+//                        final String s = tvMsg.getText().toString() + mensagemRecebida + "\n";
+//                        updateViewMensagem(s);
+//                    }
+//                }
+//            } catch (IOException e) {
+//                Log.e(TAG, "Erro no servidor: " + e.getMessage(), e);
+//                running = false;
+//            }
+//        }
+//    }
+//
+//    //Exibe a mensagem na tela, informando o nome do dispositivo que fez a conexao
+//    private void updateViewConectou(final BluetoothDevice device) {
+//        runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                TextView tvNome = (TextView) findViewById(R.id.tvNomeDispositivo);
+//                tvNome.setText(device.getName() + " - " + device.getAddress());
+//            }
+//        });
+//    }
+//
+//    //Exibe a mensagem recebida na tela (Enviada pelo outro dispositivo)
+//    private void updateViewMensagem(final String s) {
+//        runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                TextView tvMsg = (TextView) findViewById(R.id.tvMsg);
+//                tvMsg.setText(s);
+//            }
+//        });
+//    }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        running = false;
-        try {
-            if (btfSocket != null) {
-                btfSocket.close();
-            }
-            if (serverSocket != null) {
-                serverSocket.close();
-            }
-        } catch (IOException e) {
-
-        }
-    }
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        running = false;
+//        try {
+//            if (btfSocket != null) {
+//                btfSocket.close();
+//            }
+//            if (serverSocket != null) {
+//                serverSocket.close();
+//            }
+//        } catch (IOException e) {
+//
+//        }
+//    }
 
 }
